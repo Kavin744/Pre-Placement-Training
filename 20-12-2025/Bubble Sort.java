d39b4226993f774ebd4c76d2988906b6991288bd
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2025-12-20 11:49:53 +0530

    Add Bubble Sort implementation in Java

---
+++ 20-12-2025/Bubble Sort.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+        for (int i = 0; i < 5; i++)
+            arr[i] = sc.nextInt();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+        for (int n : arr)
+            System.out.print(n + " ");
+    }
+}
