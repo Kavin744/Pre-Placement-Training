65925c43e69ae75bec0f468c807072e2e709fe89
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2025-12-22 15:48:04 +0530

    Add Linear Search implementation in Java

---
+++ 21-12-2025/Linear Search.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++)
+            arr[i] = sc.nextInt();
+
+        int key = sc.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Found at index " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            System.out.println("Not Found");
+    }
+}
