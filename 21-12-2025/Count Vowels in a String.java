16bb5f20bbc2a443131afe95b47b84b5e9d8a6a3
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2025-12-22 15:49:24 +0530

    Add Vowel Count program in Java

---
+++ 21-12-2025/Count Vowels in a String.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VowelCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().toLowerCase();
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            if ("aeiou".indexOf(c) != -1)
+                count++;
+        }
+
+        System.out.println("Vowel Count = " + count);
+    }
+}
