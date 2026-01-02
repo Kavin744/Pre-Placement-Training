9bce3f692a2ac4091569a7e5a11d3a58d387078a
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2026-01-02 10:51:55 +0530

    Create hashmap.java

---
+++ 26-12-2025/hashmap.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+class MapDemo {
+    public static void main(String[] args) {
+        HashMap<Integer, String> m = new HashMap<>();
+        m.put(1, "Java");
+        m.put(2, "Python");
+        System.out.println(m);
+    }
+}
