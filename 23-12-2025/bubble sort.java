cc4ec834e02b447cd8cd5473b172808cb42851e8
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2026-01-02 10:39:42 +0530

    Add Bubble Sort implementation in Java

---
+++ 23-12-2025/bubble sort.java
@@ -0,0 +1,13 @@
+class BubbleSort {
+    public static void main(String[] args) {
+        int[] a = {5, 1, 4, 2, 8};
+        for (int i = 0; i < a.length - 1; i++)
+            for (int j = 0; j < a.length - i - 1; j++)
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+        for (int x : a) System.out.print(x + " ");
+    }
+}
