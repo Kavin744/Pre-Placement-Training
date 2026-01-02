d107dbcff41f98821ff6c1a8372f9b5e174c92f7
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2026-01-02 10:42:03 +0530

    Implement linear search algorithm in Java

---
+++ 24-12-2025/linear search.java
@@ -0,0 +1,12 @@
+class LinearSearch {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40};
+        int key = 30;
+        for (int i = 0; i < a.length; i++)
+            if (a[i] == key) {
+                System.out.println("Found at " + i);
+                return;
+            }
+        System.out.println("Not Found");
+    }
+}
