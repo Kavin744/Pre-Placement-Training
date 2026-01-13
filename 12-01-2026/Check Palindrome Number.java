81d5f4f1e12d8abf6aa9b7b9024c78d9b4fbd93a
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2026-01-13 21:14:42 +0530

    Add palindrome number checker in Java

---
+++ 12-01-2026/Check Palindrome Number.java
@@ -0,0 +1,11 @@
+public class PalindromeNum {
+    public static void main(String[] args) {
+        int n = 121, rev = 0, temp = n;
+
+        while(n > 0){
+            rev = rev*10 + n%10;
+            n /= 10;
+        }
+        System.out.println(rev == temp);
+    }
+}
