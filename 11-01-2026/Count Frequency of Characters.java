e6ac185ade66732a696627798e388a2a13f96c3b
Kavin Kumar <147464206+Kavin744@users.noreply.github.com>
2026-01-13 21:08:37 +0530

    Create Count Frequency of Characters.java

---
+++ 11-01-2026/Count Frequency of Characters.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        String s = "programming";
+        Map<Character,Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        System.out.println(map);
+    }
+}
